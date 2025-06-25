5b29981295acab139e0a690188db43e69d7228ac
rohan-kharel <111512418+rohan-kharel@users.noreply.github.com>
2025-06-25 19:16:02 +0100

    stuff

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin, TextPlugin);
+    gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin, SplitText );
 
     //Home landing shapes
     gsap.to({}, {
@@ -38,29 +38,57 @@ document.addEventListener("DOMContentLoaded", (event) => {
         }
     });
 
-    //MASSIVE card
     let cardAnimation = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.home-card-container',
-            pin: true,
-            start: 'bottom bottom',
-            end: '+=2000',
-            scrub: 1,
-        }
+
     });
         
-        cardAnimation.from('.home-card1-line-path', { drawSVG:'0%' })
-                     .to('.home-card1-line-path', { drawSVG:'100%' });
+    cardAnimation.from('.home-card1-line-path', { 
+        drawSVG:'0%',
+    })
 
-        cardAnimation.to('.home-card1-title', {
-            text: "premium coding",
-            ease: "none"
-        });
+    .to('.home-card1-line-path', { 
+        drawSVG:'100%',
+        // scrollTrigger: {
+        //     trigger: '.home-card1',
+        //     pin: true,
+        //     start: 'center center',
+        //     scrub: 1,
+        //     end: '+=589'
+        //     // anticipatePin: 1,
+        // }
+    });
 
-        cardAnimation.to('.home-card1-subtitle', {
-            text: "our sites have aerospace grade coding",
-            ease: "none"
-        }, "<" );
+    let typing = SplitText.create(".home-card2-content", { type: "chars" });
 
+    cardAnimation.from(typing.chars, {
+        autoAlpha: 0,
+        stagger: 0.02,
 
+        // scrollTrigger: {
+        //     trigger: '.home-card2',
+        //     pin: true,
+        //     start: 'center center',
+        //     scrub: 1,
+        //     end: '+=589'
+        //     // anticipatePin: 1,
+        // }
+    })
+
+    cardAnimation.from('.home-card3-line-path', { 
+        drawSVG:'0%',
+    })
+
+    .to('.home-card3-line-path', { 
+        drawSVG:'100%',
+        // scrollTrigger: {
+        //     trigger: '.home-card3',
+        //     pin: true,
+        //     start: 'center center',
+        //     scrub: 1,
+        //     end: '+=589'
+        //     // anticipatePin: 1,
+        // }
+    });
+
+    
 });
