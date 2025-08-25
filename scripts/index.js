ec6f2047b3a57cae0e0f1d1e00677547ca56d9d4
rohan-kharel <111512418+rohan-kharel@users.noreply.github.com>
2025-08-25 16:46:05 +0100

    stuff

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin, SplitText, MotionPathPlugin);
 
+    var jmediaquery = window.matchMedia( "(min-width: 981px)" );
+
     //function to handle shape animations
     async function floatingShapeAnimation(duration, className, x1, x2, y1, y2) {
         gsap.to({}, {
@@ -18,12 +20,15 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     //landing shapes
     floatingShapeAnimation(7, '.home-shape-orange' ,12, 2, 7, 2);
-    floatingShapeAnimation(5, '.home-shape-blue', 8, -2 + Math.PI / 3, 4, -2 + Math.PI / 3);
     floatingShapeAnimation(15, '.home-shape-purple', 16, 2 + Math.PI / 1.5, 10, 2 + Math.PI / 1.5);
+    if (jmediaquery.matches) {
+        floatingShapeAnimation(5, '.home-shape-blue', 8, -2 + Math.PI / 3, 4, -2 + Math.PI / 3);
 
-    floatingShapeAnimation(7, '.home-info-right-orange' ,12, 2, 7, 2);
-    floatingShapeAnimation(5, '.home-info-right-blue', 8, -2 + Math.PI / 3, 4, -2 + Math.PI / 3);
-    floatingShapeAnimation(15, '.home-info-right-purple', 16, 2 + Math.PI / 1.5, 10, 2 + Math.PI / 1.5);
+        floatingShapeAnimation(7, '.home-info-right-orange' ,12, 2, 7, 2);
+        floatingShapeAnimation(5, '.home-info-right-blue', 8, -2 + Math.PI / 3, 4, -2 + Math.PI / 3);
+        floatingShapeAnimation(15, '.home-info-right-purple', 16, 2 + Math.PI / 1.5, 10, 2 + Math.PI / 1.5);
+    }
+    
 
     //card animations
 
@@ -40,16 +45,29 @@ document.addEventListener("DOMContentLoaded", (event) => {
     });
 
     //card 1
-    let card1Animation = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.home-card1',
-            pin: true,
-            start: 'center center+=33',
-            scrub: 1,
-            end: '+=589',
-        }
-    })
+    let card1Animation;
+    if (jmediaquery.matches) {
+        card1Animation = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.home-card1',
+                pin: true,
+                start: 'center center+=33',
+                scrub: 1,
+                end: '+=589',
+            }
+        })
+    } 
     
+    else {
+        card1Animation = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.home-card1',
+                start: 'center center+=33',
+                end: '+=589'
+            }
+        })
+    }
+
     card1Animation.from('.home-card1-line-path', { 
         drawSVG:'0%',
     });
@@ -70,15 +88,26 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 
     //card 2
-    let card2Animation = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.home-card2',
-            pin: true,
-            start: 'center center+=33',
-            scrub: 1,
-            end: '+=589',
-        }
-    });
+    let card2Animation;
+    if (jmediaquery.matches) {
+        card2Animation = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.home-card2',
+                pin: true,
+                start: 'center center+=33',
+                scrub: 1,
+                end: '+=589',
+            }
+        });
+    } else {
+        card2Animation = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.home-card2',
+                start: 'center center+=33',
+                end: '+=589',
+            }
+        });
+    }
     let card2typing = SplitText.create(".home-card2-content", { type: "chars" });
 
     card2Animation.from(card2typing.chars, {
@@ -88,26 +117,27 @@ document.addEventListener("DOMContentLoaded", (event) => {
         ease: 'power2',
     });
 
-    let card2to3Animation = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.home-card3',
-            start: 'center center+=622',
-            scrub: 1,
-            end: '+=589',
-            snap: [0, 1],
-        }
-    })
-
     //card 3
-    let card3Animation = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.home-card3',
-            pin: true,
-            start: 'center center+=33',
-            scrub: 1,
-            end: '+=589',
-        },
-    });
+    let card3Animation;
+    if (jmediaquery.matches) {
+        card3Animation = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.home-card3',
+                pin: true,
+                start: 'center center+=33',
+                scrub: 1,
+                end: '+=589',
+            }
+        });
+    } else {
+        card3Animation = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.home-card3',
+                start: 'center center+=33',
+                end: '+=589',
+            }
+        });
+    }
 
     card3Animation.to('.home-card3-meter-dot', {
         motionPath: {
@@ -127,30 +157,29 @@ document.addEventListener("DOMContentLoaded", (event) => {
             innerText: 1
         },
         duration: 1,
-
     }, "<");
 
-    let card3to4Animation = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.home-card4',
-            start: 'center center+=622',
-            scrub: 1,
-            end: '+=589',
-            snap: [0, 1],
-        }
-    })
-
-
     //card 4
-    let card4Animation = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.home-card4',
-            pin: true,
-            start: 'center center+=33',
-            scrub: 1,
-            end: '+=589',
-        }
-    });
+    let card4Animation;
+    if (jmediaquery.matches) {
+        card4Animation = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.home-card4',
+                pin: true,
+                start: 'center center+=33',
+                scrub: 1,
+                end: '+=589',
+            }
+        });
+    } else {
+        card4Animation = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.home-card4',
+                start: 'center center+=33',
+                end: '+=589',
+            }
+        });
+    }
 
     card4Animation.to('.server-indicator-blue', {
         opacity: 1,
@@ -164,49 +193,65 @@ document.addEventListener("DOMContentLoaded", (event) => {
         opacity: 1,
     });
 
-    let card4to5Animation = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.home-card5',
-            start: 'center center+=622',
-            scrub: 1,
-            end: '+=589',
-            snap: [0, 1],
-        }
-    })
-
     //card 5
+    let card5Animation;
+    if (jmediaquery.matches) {
+        card5Animation = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.home-card5',
+                pin: true,
+                start: 'center center+=33',
+                scrub: 1,
+                end: '+=589',
+            }
+        });
 
-    let card5Animation = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.home-card5',
-            pin: true,
-            start: 'center center+=33',
-            scrub: 1,
-            end: '+=589',
-        }
-    });
-    let card5typing = SplitText.create(".home-card5-searchbar-text", { type: "chars" });
+        let card5typing = SplitText.create(".home-card5-searchbar-text", { type: "chars" });
 
-    card5Animation.from(card5typing.chars, {
-        autoAlpha: 0,
-        stagger: 1,
-        ease: 'power2',
+        card5Animation.from(card5typing.chars, {
+            autoAlpha: 0,
+            stagger: 1,
+            ease: 'power2',
+            duration: 1,
+        });
 
-        duration: 1,
-    });
+        card5Animation.to('.home-card5-results', {
+            opacity: 1,
+            duration: 5,
+        }); 
 
-    card5Animation.to('.home-card5-results', {
-        opacity: 1,
+    } else {
+        card5Animation = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.home-card5',
+                start: 'center center+=33',
+                end: '+=589',
+            }
+        });
 
-        duration: 5,
-    }); 
+        let card5typing = SplitText.create(".home-card5-searchbar-text", { type: "chars" });
 
-    let infoShapesAnimation = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.home-info1-right',
-            pin: true,
-            start: 'center center+=33',
-            end: '+=530',
-        }
-    })
+        card5Animation.from(card5typing.chars, {
+            autoAlpha: 0,
+            stagger: 0.05,
+            ease: 'power2',
+            duration: 0.5,
+        });
+
+        card5Animation.to('.home-card5-results', {
+            opacity: 1,
+            duration: 1,
+        }); 
+    }
+
+    if (jmediaquery.matches) {
+        let infoShapesAnimation = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.home-info1-right',
+                pin: true,
+                start: 'center center+=33',
+                end: '+=530',
+            }
+        })
+    }
 });
